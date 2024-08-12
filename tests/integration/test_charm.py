d520e792da835c4a540486c1679f38b5a477ae54
Will French <will.french@canonical.com>
2024-08-12 09:27:44 -0500

    Improve integration test coverage, clean up naming

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -8,6 +8,8 @@ from pathlib import Path
 
 import pytest
 import yaml
+from lightkube import AsyncClient
+from lightkube.resources.core_v1 import Node
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
@@ -29,6 +31,62 @@ async def test_build_and_deploy(ops_test: OpsTest):
     await asyncio.gather(
         ops_test.model.deploy(charm, application_name=APP_NAME, trust=True),
         ops_test.model.wait_for_idle(
-            apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
+            apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1800
         ),
     )
+
+
+async def test_charm_status(ops_test: OpsTest):
+    application = ops_test.model.applications[APP_NAME]
+    units = application.units
+    cert_manager_latest_version = Path("upstream", "cert-manager", "version").read_text().strip()
+    operator_latest_version = (
+        Path("upstream", "intel-device-plugins-operator", "version").read_text().strip()
+    )
+    assert units[0].workload_status == "active"
+    assert units[0].workload_status_message == "Ready"
+    assert application.status == "active"
+    assert (
+        application.workload_version == f"{cert_manager_latest_version},{operator_latest_version}"
+    )
+
+
+async def test_node_label(kubernetes: AsyncClient):
+    """Verify that expected node label is present.
+
+    This will fail if an Intel GPU is not present on the system.
+    """
+    async for node in kubernetes.list(Node):
+        assert node.metadata.labels["intel.feature.node.kubernetes.io/gpu"] == "true"
+
+
+async def test_node_status(kubernetes: AsyncClient):
+    """Verify that the number of GPU slots are the expected value."""
+    async for node in kubernetes.list(Node):
+        # The plugin is configured to allow up to 10 containers
+        # access to each GPU
+        slots_per_gpu = 10
+        gpu_count = sum(
+            [
+                int(val)
+                for key, val in node.metadata.labels.items()
+                if key.startswith("gpu.intel.com/device-id.") and key.endswith(".count")
+            ]
+        )
+        slots = gpu_count * slots_per_gpu
+        assert node.status.capacity["gpu.intel.com/i915"] == str(slots)
+        assert node.status.allocatable["gpu.intel.com/i915"] == str(slots)
+
+
+async def test_adjust_version(ops_test: OpsTest):
+    """Verify that the application versions can be adjusted."""
+    update_status_timeout = 1800
+    application = ops_test.model.applications[APP_NAME]
+    await application.set_config({"intel-device-plugins-operator-release": "0.29.0"})
+    await ops_test.model.wait_for_idle(status="active", timeout=update_status_timeout)
+    await application.reset_config(["intel-device-plugins-operator-release"])
+    await ops_test.model.wait_for_idle(status="active", timeout=update_status_timeout)
+    await application.set_config({"cert-manager-release": "v1.14.5"})
+    await ops_test.model.wait_for_idle(status="active", timeout=update_status_timeout)
+    await application.reset_config(["cert-manager-release"])
+    await ops_test.model.wait_for_idle(status="active", timeout=update_status_timeout)
--- tests/unit/test_manifests_operator.py
+++ tests/unit/test_manifests_operator.py
@@ -4,14 +4,14 @@
 import unittest.mock as mock
 
 import pytest
-from charm import IntelDevicePluginsK8SOperatorCharm
+from charm import IntelDevicePluginsK8sCharm
 from manifests_operator import PatchPluginName
 from ops.testing import Harness
 
 
 @pytest.fixture
 def harness():
-    harness = Harness(IntelDevicePluginsK8SOperatorCharm)
+    harness = Harness(IntelDevicePluginsK8sCharm)
     try:
         harness.begin()
         harness.set_leader(is_leader=True)
@@ -23,8 +23,8 @@ def harness():
 def test_patch_plugin_name(harness: Harness):
     patch = PatchPluginName(harness.charm.collector.manifests)
     obj = mock.MagicMock()
-    d = {"kind": "GpuDevicePlugin", "metadata": {"name": "dummydeviceplugin-sample"}}
+    d = {"kind": "GpuDevicePlugin", "metadata": {"name": "mockdeviceplugin-sample"}}
     obj.__getitem__.side_effect = d.__getitem__
     obj.kind = d["kind"]
     patch(obj)
-    assert obj["metadata"]["name"] == "dummydeviceplugin"
+    assert obj["metadata"]["name"] == "mockdeviceplugin"
